6fc871ac539d5ba05a03328b56b3691babbfe71f
神代綺凜 <jindaikirin@ykilin.moe>
2018-06-07 01:06:30 +0800

    Add files via upload

---
+++ centos_install_python3.6.2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+yum -y groupinstall "Development tools"
+yum -y install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel
+wget https://www.python.org/ftp/python/3.6.2/Python-3.6.2.tar.xz
+tar -xvJf  Python-3.6.2.tar.xz
+cd Python-3.6.2
+./configure --prefix=/usr/local/python3
+make && make install
+ln -s /usr/local/python3/bin/python3 /usr/bin/python3
+ln -s /usr/local/python3/bin/pip3 /usr/bin/pip3
